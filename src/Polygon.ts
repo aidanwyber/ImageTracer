ae005b188ad5afaa12056b7d5dcf75b1100a7d21
Aidan Wyber <pandakaas@gmail.com>
2025-10-15 12:35:14 +0200

    Update Polygon.ts

--- src/Polygon.ts
+++ src/Polygon.ts
@@ -1,7 +1,7 @@
-export class Polygon {
-	constructor(points: Point[]) {}
-	flipVertexOrder() {}
-	isClockwise(): boolean {}
-	getCircumference(): number {}
-	reduceVertices(minLen: number) {}
-}
+// export class Polygon {
+// 	constructor(points: Point[]) {}
+// 	flipVertexOrder() {}
+// 	isClockwise(): boolean {}
+// 	getCircumference(): number {}
+// 	reduceVertices(minLen: number) {}
+// }
