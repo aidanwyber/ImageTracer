1f8f95d2ffcd9fd49e7f420657c6a39987b61b89
Aidan Wyber <pandakaas@gmail.com>
2025-10-20 17:09:25 +0200

    formatting and styling

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -4,19 +4,19 @@ import { defineConfig } from 'vite';
 const demoRoot = resolve(__dirname);
 
 export default defineConfig({
-        root: demoRoot,
-        publicDir: resolve(__dirname, 'public'),
-        build: {
-                outDir: resolve(__dirname, 'dist'),
-                emptyOutDir: true,
-                rollupOptions: {
-                        input: resolve(__dirname, 'index.html'),
-                },
-        },
-        server: {
-                open: true,
-                fs: {
-                        allow: [resolve(__dirname, '..')],
-                },
-        },
+	root: demoRoot,
+	publicDir: resolve(__dirname, 'public'),
+	build: {
+		outDir: resolve(__dirname, 'dist'),
+		emptyOutDir: true,
+		rollupOptions: {
+			input: resolve(__dirname, 'index.html'),
+		},
+	},
+	server: {
+		open: true,
+		fs: {
+			allow: [resolve(__dirname, '..')],
+		},
+	},
 });
